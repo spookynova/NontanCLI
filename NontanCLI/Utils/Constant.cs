6c7d3f04df180f8c9e176a1c408c8ab097a8a5b7
evnxwashere <evnx32@gmail.com>
2023-04-23 01:21:22 +0700

    Setup Constant and Config

--- NontanCLI/Utils/Constant.cs
+++ NontanCLI/Utils/Constant.cs
@@ -1,19 +1,49 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using NontanCLI.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace NontanCLI.Utils
 {
     public class Constant
     {
         public static string BaseUrl = "https://api.consumet.org/";
-        public static string CORS = "https://m3u8-proxy-imkryp70n.vercel.app/cors?url=";
-
-        // config
+        public static string CORS = "https://m3u8-proxy-murex.vercel.app/cors?url=";
 
         public static string ConfigPath = "config.json";
+        public static string PORT = "";
+        public static string baseAddress = "";
+        public static string provider = "";
 
+        
+        public static void InitConfig()
+        {
+            // Check if config.json file exists
+            if (File.Exists(ConfigPath))
+            {
+                string configJson = File.ReadAllText(ConfigPath);
+                ConfigModel config = JsonConvert.DeserializeObject<ConfigModel>(configJson);
+
+                // Access the configuration data
+                PORT = config.port;
+                baseAddress = "http://localhost:" + PORT + "/";
+                provider = config.provider.ToLower();
+            } else
+            {
+                // Create and write JSON content to config.json
+                ConfigModel config = new ConfigModel
+                {
+                    port = "8000",
+                    provider = "gogoanime"
+                };
+                string configFileContent = JsonConvert.SerializeObject(config, Formatting.Indented);
+                File.WriteAllText(Constant.ConfigPath, configFileContent);
+                Console.WriteLine("config.json file created with default values.");
+            }
+        }
     }
 }
